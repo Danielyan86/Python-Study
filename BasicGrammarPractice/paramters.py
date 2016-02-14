ead342782018b0f31a9c35b9deb3bc541423e43d
xiaodong.yan <xiaodong.yan@tubemogul.com>
2016-02-14 14:19:57 +0800

    update parameter practice

--- BasicGrammarPractice/paramters.py
+++ BasicGrammarPractice/paramters.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Created on 2013-11-28
 
@@ -20,3 +21,24 @@ parameter_usage(A, list_A)
 
 # list A as seperate parameter in the fisrt element in para2 list
 parameter_usage(A, *list_A)
+
+# 这里记住的是类型是属于对象的，而不是变量。而对象有两种,“可更改”（mutable）与“不可更改”（immutable）对象。在python中，strings, tuples,
+# 和numbers是不可更改的对象，而list,dict等则是可以修改的对象。(这就是这个问题的重点)
+
+a = 1
+
+
+def fun(a):
+    a = 2
+
+
+print a  # 1
+
+a = []
+
+
+def fun(a):
+    a.append(1)
+
+
+print a  # [1]
