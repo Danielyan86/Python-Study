d3cfd58c4909b67d5deb44136272c19f062a3d78
Danielyan86 <516495459@qq.com>
2016-06-21 15:59:33 +0800

    refactor queue function

--- Python-algorithms/QueueStack/queue.py
+++ Python-algorithms/QueueStack/queue.py
@@ -1,22 +1,21 @@
 def queue(string_para):
-    counter = 1
     string_new = ''
     string_old = string_para
 
     while 1:
         if string_old:
-            if counter == 1:
+            if len(string_old) == 1:
+                string_new = string_new + string_old[0]
+                string_old = ""
+            else:
                 string_new = string_new + string_old[0]
                 string_old = string_old[1:]
-                counter = 0
-            elif counter == 0:
                 string_old = string_old[1:] + string_old[0]
-                counter = 1
-            print string_old, string_new
+                print string_old, string_new
         else:
             break
     return string_new
 
 
 if __name__ == '__main__':
-    print queue("631758924")
+    print queue("63175892443")
