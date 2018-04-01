79d56b2f9a7014bcc7f3f962f8fe63293100bb82
Danielyan86 <516495459@qq.com>
2018-04-01 19:24:21 +0800

    update

--- GrammarPractice/Basic/String_basic.py
+++ GrammarPractice/Basic/String_basic.py
@@ -14,3 +14,16 @@ print(a[:-1])
 print("this is %s" % 'A')
 
 print("this is {0}, {1}".format('A', 'B'))
+
+b = r'''
+fdsfkjdkf
+dfsdfds'
+fsdf"
+\n
+'''
+print(b)
+
+if "21" in a:
+    print(True)
+else:
+    print(False)
