f39526868013efae58fbae1dea147b030e9ec97b
Danielyan86 <516495459@qq.com>
2018-05-06 19:07:16 +0800

    update

--- GrammarPractice/Basic/module_import.py
+++ GrammarPractice/Basic/module_import.py
@@ -1,8 +1,11 @@
 import math
+from math import fabs
 
 import numpy as nm
 
-from math import fabs
-
 print(math, nm)
 print(fabs(-1))
+
+import GrammarPractice.Basic.function_return as fr
+
+fr.hello()
