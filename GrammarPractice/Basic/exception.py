c10ed6fdaf13d074fe2180a4742ec98d8f978395
Danielyan86 <516495459@qq.com>
2018-05-06 10:31:27 +0800

    update

--- GrammarPractice/Basic/exception.py
+++ GrammarPractice/Basic/exception.py
@@ -2,7 +2,8 @@
 def catch_specific_exception():
     try:
         f = open("blala", 'r')
-    except IOError as e:
+    except FileNotFoundError as e:
+        print(e)
         print("could not open the file")
 
     try:
@@ -25,25 +26,36 @@ def catch_all_exceptions():
     try:
         f = open("blala", 'r')
     except Exception as e:
+        print(e)
         print("could not open the file")
+        # raise
     finally:
         print("this is the end")
 
 
 def asser_keyword():
     assert 1 == 1
+    # assert 1 == 0
 
-
-# assert 1 == 0
 
 # raise触发异常
 # raise ZeroDivisionError("error")
 def raise_function(information=None):
-    # print("throw out the exception")
+    print("throw out the exception")
+    raise NameError('HiThere')
     # raise AssertionError("throw out the exception")
     # raise RuntimeError("run time error")
-    pass
+
+
+def raise_function2():
+    try:
+        raise NameError('HiThere')
+    except NameError:
+        print('An exception flew by!')
+        raise
 
 
 if __name__ == '__main__':
-    raise_function()
+    # catch_specific_exception()
+    # raise_function()
+    catch_all_exceptions()
