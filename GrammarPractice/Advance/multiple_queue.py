d9426f8740d6b350e076f102e2cd6480901dcbc4
Danielyan86 <516495459@qq.com>
2018-05-20 00:02:52 +0800

    [Update]

--- GrammarPractice/Advance/multiple_queue.py
+++ GrammarPractice/Advance/multiple_queue.py
@@ -1,5 +1,6 @@
+import threading
 from queue import Queue
-from github import Github
+
 import requests
 
 
@@ -17,26 +18,56 @@ def queue_basic():
 
 
 class spider:
-    def get_item(self):
-        pass
+    q = Queue()
 
-    def read_and_write_to_file(self):
-        pass
+    def __init__(self):
+        self.req = requests.Session()
+
+    def get_user_list(self):
+        url = "https://api.github.com/orgs/PlayPython/members?access_token=7e0fa14fd501521080b0103c2bc82a1a5189389f"
+        res = self.req.get(url)
+        self.user_urls = [item['url'] for item in res.json()]
+
+    def one_thread_get_public_repos(self):
+        res_list = []
+        for url in self.user_urls:
+            res = self.req.get(url + "?access_token=7e0fa14fd501521080b0103c2bc82a1a5189389f")
+            res_list.append(res.json()['public_repos'])
+        print(res_list)
+
+    def get_function(self, url):
+        print(url)
+        res = self.req.get(url + "?access_token=7e0fa14fd501521080b0103c2bc82a1a5189389f")
+        print(res.json()['public_repos'])
+        self.q.put(res.json()['public_repos'])
+
+    def multiple_thread_get_public_repos(self):
+        threads = []
+        for url in self.user_urls:
+            threads.append(threading.Thread(target=self.get_function, args=(url,)))
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+
+    def read_queue_write_to_file(self):
+        while not self.q.empty():
+            print(self.q.get())
+            # print(q.get())
 
 
 if __name__ == '__main__':
-    # queue_basic()
-    # or using an access token
-    # g = Github("97c10cdf15ef4442463bc4aad511e234c5943932")
-    # Github Enterprise with custom hostname
-    # g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
+    spider_inst = spider()
+    spider_inst.get_user_list()
+    spider_inst.one_thread_get_public_repos()
 
-    # Then play with your Github objects:
-    # url = "https://api.github.com/user/orgs?access_token=97c10cdf15ef4442463bc4aad511e234c5943932"
-    url = "https://api.github.com/orgs/PlayPython/members?access_token=97c10cdf15ef4442463bc4aad511e234c5943932"
-    req = requests.Session()
-    res = req.get(url)
-    print(
-        res.json())
-    for item in res.json():
-        print(item)
+    # spider_inst.multiple_thread_get_public_repos()
+    # spider_inst.read_queue_write_to_file()
+# queue_basic()
+# or using an access token
+# g = Github("97c10cdf15ef4442463bc4aad511e234c5943932")
+# Github Enterprise with custom hostname
+# g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
+
+# Then play with your Github objects:
+# url = "https://api.github.com/user/orgs?access_token=97c10cdf15ef4442463bc4aad511e234c5943932"
--- GrammarPractice/Advance/multiplethread_join.py
+++ GrammarPractice/Advance/multiplethread_join.py
@@ -12,17 +12,17 @@ def thread_fuc(info=None):
     print("end time", ctime())
 
 
-def multiple_fuc():
-    threads = [threading.Thread(target=thread_fuc, args=("hello",)) for i in range(10)]
+def multiple_fuc(fun):
+    threads = [threading.Thread(target=fun, args=("hello",)) for i in range(10)]
     print(threads)
     for th in threads:
         print(th)
         th.start()
-    for th in threads:
-        th.join()
+    # for th in threads:
+    #     th.join()  # join 函数会等到线程结束
 
 
 if __name__ == '__main__':
-    multiple_fuc()
-    # for i in range(10):
-    #     thread_fuc()
+    multiple_fuc(thread_fuc())
+    for i in range(10):
+        thread_fuc()
