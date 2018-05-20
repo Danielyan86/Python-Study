6ce6f6eac4f0f3402e6a102370aa3f15e8b673dd
Danielyan86 <516495459@qq.com>
2018-05-20 14:45:07 +0800

    update python-study

--- GrammarPractice/Advance/multiple_queue.py
+++ GrammarPractice/Advance/multiple_queue.py
@@ -24,20 +24,20 @@ class spider:
         self.req = requests.Session()
 
     def get_user_list(self):
-        url = "https://api.github.com/orgs/PlayPython/members?access_token=7e0fa14fd501521080b0103c2bc82a1a5189389f"
+        url = "https://api.github.com/orgs/PlayPython/members?access_token=376d764af54ded124fe3f241309e090c5b58b18e"
         res = self.req.get(url)
         self.user_urls = [item['url'] for item in res.json()]
 
     def one_thread_get_public_repos(self):
         res_list = []
         for url in self.user_urls:
-            res = self.req.get(url + "?access_token=7e0fa14fd501521080b0103c2bc82a1a5189389f")
+            res = self.req.get(url + "?access_token=376d764af54ded124fe3f241309e090c5b58b18e")
             res_list.append(res.json()['public_repos'])
         print(res_list)
 
     def get_function(self, url):
         print(url)
-        res = self.req.get(url + "?access_token=7e0fa14fd501521080b0103c2bc82a1a5189389f")
+        res = self.req.get(url + "?access_token=376d764af54ded124fe3f241309e090c5b58b18e")
         print(res.json()['public_repos'])
         self.q.put(res.json()['public_repos'])
 
@@ -59,15 +59,7 @@ class spider:
 if __name__ == '__main__':
     spider_inst = spider()
     spider_inst.get_user_list()
-    spider_inst.one_thread_get_public_repos()
+    # spider_inst.one_thread_get_public_repos()
 
-    # spider_inst.multiple_thread_get_public_repos()
-    # spider_inst.read_queue_write_to_file()
-# queue_basic()
-# or using an access token
-# g = Github("97c10cdf15ef4442463bc4aad511e234c5943932")
-# Github Enterprise with custom hostname
-# g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
-
-# Then play with your Github objects:
-# url = "https://api.github.com/user/orgs?access_token=97c10cdf15ef4442463bc4aad511e234c5943932"
+    spider_inst.multiple_thread_get_public_repos()
+    spider_inst.read_queue_write_to_file()
