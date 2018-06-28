56ee8a305e8bc1a79c5b2a4ec3fb25ae591928f6
Danielyan86 <516495459@qq.com>
2018-06-28 16:12:48 +0800

    update python

--- Python-algorithms/Enumeration/example1.py
+++ Python-algorithms/Enumeration/example1.py
@@ -1,5 +1,10 @@
 # 题目，1到9不重复的9个数字，使得三位数加三位数等式成立
-# 暴力穷举算法, 此方法
+
+import itertools
+import time
+
+# 暴力穷举算法, 此方法效率极低，需要半个多小时
+start = time.clock()
 for number in range(123456789, 987654321):
     flag = True
     for j in range(1, 10):  # 判断有没有重复的数字
@@ -11,11 +16,30 @@ for number in range(123456789, 987654321):
         if int(number_string[0:3]) + int(number_string[3:6]) == int(number_string[6:]) and int(
                 number_string[0:3]) < 500:
             print("{0}+{1}={2}".format(number_string[0:3], number_string[3:6], number_string[6:]))
+end = time.clock()
+print('Running time: %s Seconds' % (end - start))
 
+start = time.clock()
+for num in range(123, 500):
+    for num2 in range(123, 500):
+        add_result = num + num2
+        all_num_string = "{0}{1}{2}".format(num, num2, add_result)
+        flag = True
+        if add_result < 1000:
+            for j in range(1, 10):  # 判断有没有重复的数字
+                if str(j) not in all_num_string:
+                    flag = False
+                    break
+        if flag:
+            print("{0}+{1}={2}".format(num, num2, add_result))
+end = time.clock()
+print('Running time: %s Seconds' % (end - start))
+
+start = time.clock()
 # 优化后的方案采用全排列的思想, 大大执行提高效率
-import itertools
-
-for item in itertools.permutations("123456789", 9):
-    number_string = "".join((item))
+for item in itertools.permutations("123456789", 9):  # 全排列方法permutations
+    number_string = "".join(item)  # 字符串转化成列表
     if int(number_string[0:3]) + int(number_string[3:6]) == int(number_string[6:]) and int(number_string[0:3]) < 500:
         print("{0}+{1}={2}".format(number_string[0:3], number_string[3:6], number_string[6:]))
+end = time.clock()
+print('Running time: %s Seconds' % (end - start))
--- Python-algorithms/data_structure/collection.py
+++ Python-algorithms/data_structure/collection.py
@@ -6,15 +6,20 @@ beer_card = Card('7', 'diamonds')
 print(type(beer_card))
 print(beer_card)
 
+City = collections.namedtuple('city', 'name country population coordinates')
+chengdu = City('成都', '中国', 36.993, (35, 139))
+print(chengdu)
+print(chengdu.population)
+print(chengdu[1])
 
-class FrenchDeck:
-    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
-    suits = 'spades diamonds clubs hearts'.split()
-
-    def __init__(self):
-        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
-
-    def __len__(self):
-        return len(self._cards)
-
-    def __getitem__(self, position): return self._cards[position]
+# class FrenchDeck:
+#     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+#     suits = 'spades diamonds clubs hearts'.split()
+#
+#     def __init__(self):
+#         self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+#
+#     def __len__(self):
+#         return len(self._cards)
+#
+#     def __getitem__(self, position): return self._cards[position]
