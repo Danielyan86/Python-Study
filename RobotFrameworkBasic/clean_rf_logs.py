be076bec16f239a4dccb02276c322e2372b2c666
Danielyan86 <516495459@qq.com>
2018-07-09 10:36:21 +0800

    Revert "Revert "update clean up script""
    This reverts commit 13118724a67e9d721891d857e8441aba7830537a.

--- RobotFrameworkBasic/clean_rf_logs.py
+++ RobotFrameworkBasic/clean_rf_logs.py
@@ -1,12 +1,15 @@
 import os
 import re
 
-print(os.listdir("."))
+print(os.walk("."))
 
 pattern = re.compile(r"(.*\.html|.*\.xml)")
-for file in os.listdir("."):
-    res = pattern.search(file)
-    if res:
-        print(res.group())
-
-    os.system("rm {0}".format(file))
+for root, dirs, file_list in os.walk("."):
+    # print(root, dirs, file_list)
+    for file in file_list:
+        path = os.path.join(root, file)
+        print(path)
+        res = pattern.search(path)
+        if res:
+            print(res.group())
+            os.system("rm {0}".format(path))
