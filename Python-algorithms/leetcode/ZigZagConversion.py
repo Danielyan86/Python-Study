83c27ba7686d06fa27fb1977b772f3ff14436f05
Danielyan86 <516495459@qq.com>
2018-07-19 17:45:35 +0800

    update python

--- Python-algorithms/leetcode/ZigZagConversion.py
+++ Python-algorithms/leetcode/ZigZagConversion.py
@@ -49,41 +49,36 @@ class Solution:
     def convert(self, s, numRows):
         if numRows == 1:
             return s
-        res_s = ""
-        str_len = len(s)
+        if numRows >= len(s):
+            return s
+        if numRows > 1:
+            res_s = ""
+            str_len = len(s)
 
-        for i in range(0, numRows):
-            start_p = i  # 每一行第一个字符输出位置
-            position = 0
-            print(res_s)
-            while True:
-                if i == 0 or i == numRows - 1:
-                    print(start_p)
-
-                    res_s = res_s + s[start_p]
-                    start_p = start_p + numRows * 2 - 2
-                    if start_p >= str_len:
-                        break
-                else:
-                    res_s = res_s + s[start_p]
-                    if i <= str_len // 2:
+            for i in range(0, numRows):
+                start_p = i  # 每一行第一个字符输出位置
+                position = 0
+                print(res_s)
+                while True:
+                    if i == 0 or i == numRows - 1:  # 第一行或者最后一行每个字母间隔相等
+                        print(start_p)
+                        res_s = res_s + s[start_p]
+                        start_p = start_p + numRows * 2 - 2
+                        if start_p >= str_len:
+                            break
+                    else:
+                        res_s = res_s + s[start_p]
                         if position % 2 == 0:  # 偶数位和奇数位不一样
                             start_p = start_p + (numRows - (i + 1)) * 2
                         else:
                             start_p = start_p + 2 * i
                         position = position + 1
-                    elif i > str_len // 2:
-                        if position % 2 != 0:  # 偶数位和奇数位不一样
-                            start_p = start_p + (numRows - (i + 1)) * 2
-                        else:
-                            start_p = start_p + 2 * i
-                        position = position + 1
-                    if start_p >= str_len:
-                        break
-        return res_s
+                        if start_p >= str_len:
+                            break
+            return res_s
 
 
 if __name__ == '__main__':
     s_obj = Solution()
-    res = s_obj.convert("PAYPALISHIRING", 3)
+    res = s_obj.convert("ABCDEF", 5)
     print(res)
