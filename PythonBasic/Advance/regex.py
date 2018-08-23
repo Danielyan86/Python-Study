d2dfac937849605448fc8b796781aab28af924cb
Danielyan86 <516495459@qq.com>
2018-08-23 14:30:55 +0800

    update python

--- PythonBasic/Advance/regex.py
+++ PythonBasic/Advance/regex.py
@@ -1,57 +1,95 @@
+'''
+match()	Determine if the RE matches at the beginning of the string.
+search()	Scan through a string, looking for any location where this RE matches.
+findall()	Find all substrings where the RE matches, and returns them as a list.
+finditer()	Find all substrings where the RE matches, and returns them as an iterator.
+'''
 import re
 
 print("=" * 20, "match", "=" * 20)
 # 将正则表达式编译成Pattern对象
 pattern = re.compile(r'hello')
 
-# 使用Pattern匹配文本，获得匹配结果，无法匹配时将返回None
-match = pattern.match('hello world!')
 
-if match:
-    # 使用Match获得分组信息, group 方法返回匹配正则表达式字符串
-    print(match.group(), type(match.group()))
-    print(match.groups())
-    print(match.start(), match.end())
+def match_function():
+    # 使用Pattern匹配文本，获得匹配结果，无法匹配时将返回None
+    match = pattern.match('hello world!')
 
-# 匹配失败
-result = pattern.match('!hello world!')
-print(result)
-print("=" * 20, "search", "=" * 20)
+    if match:
+        # 使用Match获得分组信息, group 方法返回匹配正则表达式字符串
+        print(match.group(), type(match.group()))
+        print(match.groups())
+        print(match.start(), match.end())
 
-# search
-result = pattern.search('!hello world!hello')
-print(result.group())
+    # 若匹配失败,result为None
+    result = pattern.match('!hello world!')
+    print(result)
+    print("=" * 20, "search", "=" * 20)
 
-print("=" * 20, "Groups", "=" * 20)
-# Groups返回匹配（）里面的字符串
-pattern = re.compile(r'hello(.*) world!')
-res = pattern.search("!hello , beautiful world!")
-print(res.group())
-print(res.groups())
 
-print("=" * 20, "findall", "=" * 20)
-# 用findall() 找到每个匹配出现的部分，与search不同的是，findall总是返回一个列表
-p = re.compile(r'\d+')
-res = p.findall('12 drummers drumming, 11 pipers piping, 10 lords a-leaping')
-print(res)
+def search_function():
+    # search 若匹配失败,result为None
+    global pattern
+    result = pattern.search('!hello world!hello')
+    print(result.group())  # gourp()方法返回一个字符串
 
-# finditer() 返回一个迭代器
-# span() 返回一个元组，包含匹配字符串的开始和结束位置的索引
-iterator = p.finditer('12 drummers drumming, 11 ... 10 ...')
-for match in iterator:
-    print(match.span())
-    print(match.group())
+    print("=" * 20, "Groups", "=" * 20)
 
-print("=" * 20, "raw", "=" * 20)
+    pattern = re.compile(r'hello(.*) world!')
+    res = pattern.search("!hello , beautiful world!")
+    print(res.group())
+    print(res.groups())  # groups 返回一个元组
 
-# 字符串加r表示原始字符串
-print("\n")
-print(r"\n")
 
-pattern = re.compile("\w+")
-string = r'hello \n world!'
-string2 = 'hello \n world!'
-res = pattern.findall(string)
-print(res)
-res = pattern.findall(string2)
-print(res)
+def findall_function():
+    # 用findall() 找到每个匹配出现的部分，与search不同的是，findall总是返回一个列表
+    p = re.compile(r'\d+')
+    res = p.findall('12 drummers drumming, 11 pipers piping, 10 lords a-leaping')
+    print(res)
+
+
+def finditer_function():
+    # finditer() 返回一个迭代器
+    # span() 返回一个元组，包含匹配字符串的开始和结束位置的索引
+    p = re.compile(r'\d+')
+    iterator = p.finditer('12 drummers drumming, 11 ... 10 ...')
+    for match in iterator:
+        print(match.span())
+        print(match.group())
+
+
+def raw_example():
+    print("=" * 20, "raw", "=" * 20)
+
+    # 字符串加r表示原始字符串
+    print("\n")
+    print(r"\n")
+
+    pattern = re.compile("\w+")
+    string = r'hello \n world!'
+    string2 = 'hello \n world!'
+    res = pattern.findall(string)
+    print(res)
+    res = pattern.findall(string2)
+    print(res)
+
+
+def or_in_patttern():
+    pattern = re.compile(r'"(htt.*jpg.*) class|(htt.*jpg.*)\)"')
+    res = pattern.search(
+        '''
+        '<div class="img"><div data-background="http://static1.keepcdn.com/picture/2018/07/15/20/f09a562a22592fab1ef598517eb04ca222832e9a_1335x1334.jpg?imageMogr2/thumbnail/306x/quality/95" class="keep-lazy"></div></div>'
+        ''')
+    group = res.group()
+
+    print(group, type(group))  # group 里面匹配的是整个字符串
+    print(res.groups())  # group 是只包含括号里面的内容,通常用此方法取括号里面匹配的内容
+
+
+if __name__ == '__main__':
+    # match_function()
+    # search_function()
+    # findall_function()
+    # finditer_function()
+    # raw_example()
+    or_in_patttern()
