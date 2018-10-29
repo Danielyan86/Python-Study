bcb20e7a895a3229033a01963ff35b210151f401
Danielyan86 <516495459@qq.com>
2018-10-29 15:37:23 +0800

    add automation test docker demo

---
+++ DockerDemo/automation_test/unittest_demo/README.md
@@ -0,0 +1,3 @@
+# 详细教程
+- https://testerhome.com/topics/16605
+- https://zhuanlan.zhihu.com/p/41945353
--- DockerDemo/automation_test/unittest_demo/bing_test.py
+++ DockerDemo/automation_test/unittest_demo/bing_test.py
@@ -5,11 +5,9 @@ from selenium import webdriver
 
 class TestBingHomepage(unittest.TestCase):
     REMOTE = True  # if the REMOTE value is true, the test runs in docker UI environment
+    # REMOTE = False
 
-    def test_bing(self):
-        print("start test")
-        bing_url = "https://bing.com"
-
+    def setUp(self):
         if self.REMOTE:
             selenium_grid_url = "http://127.0.0.1:4444/wd/hub"
             capabilities = webdriver.DesiredCapabilities.CHROME.copy()
@@ -17,9 +15,16 @@ class TestBingHomepage(unittest.TestCase):
         else:
             self.d = webdriver.Chrome()
             self.d.implicitly_wait(10)
+
+    def test_bing(self):
+        print("start test")
+        bing_url = "https://bing.com"
+
         self.d.get(bing_url)
+        self.d.find_element_by_id("sb_form_q")  # 验证输入框
         self.d.implicitly_wait(10)
 
+    # 关闭浏览器
     def tearDown(self):
         time.sleep(10)
         self.d.close()
