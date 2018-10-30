c9fa826b11566cc0f39c56f858d249306c533d90
Danielyan86 <516495459@qq.com>
2018-10-30 14:10:59 +0800

    update python

--- PythonBasic/Basic/exception.py
+++ PythonBasic/Basic/exception.py
@@ -1,11 +1,10 @@
 # 捕获指定异常
 def catch_specific_exception():
     try:
-        f = open("blala", 'r')
+        open("blala", 'r')
     except FileNotFoundError as e:
-        print(e)
-        print("could not open the file")
-
+        print(e)  # 打印系统标准异常信息
+        print("could not open the file")  # 打印自定义信息
     try:
         1 / 0
     except ZeroDivisionError as e:
@@ -17,7 +16,8 @@ def safe_float(obj):
     try:
         retval = float(obj)
     except (ValueError, TypeError):
-        retval = "argument must be a number or tring"
+        retval = "argument must be a number or string"
+    print(retval)
     return retval
 
 
@@ -33,7 +33,8 @@ def catch_all_exceptions():
         print("this is the end")
 
 
-def asser_keyword():
+# 使用断言关键字抛出异常
+def assert_keyword():
     assert 1 == 1
     # assert 1 == 0
 
@@ -49,13 +50,17 @@ def raise_function(information=None):
 
 def raise_function2():
     try:
-        raise NameError('HiThere')
+        raise NameError('HiThere')  # 抛出异常
     except NameError:
         print('An exception flew by!')
-        raise
+        raise  # 抛出捕获的异常
 
 
 if __name__ == '__main__':
+    # safe_float("1")
     # catch_specific_exception()
     # raise_function()
     catch_all_exceptions()
+    assert_keyword()
+    # raise_function()
+    raise_function2()
