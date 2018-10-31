5a875e1054c14615b0b442d2244e4db2f28df62e
Danielyan86 <516495459@qq.com>
2018-10-31 17:03:35 +0800

    update python

--- PythonTutorial/Advance/multipleprocessing.py
+++ PythonTutorial/Advance/multipleprocessing.py
@@ -1,5 +1,6 @@
 # multiprocessing is a package that supports spawning processes using an API similar to the threading module
 # multiprocessing是一个高级的多线程模块，里面封装了threading 模块
+# 对多核CPU的利用率会比threading好的多。
 from multiprocessing import Pool, Process
 import os
 
@@ -13,15 +14,24 @@ def f(x):
 
 # 经过测试，提高process线程数并不能提高程序运行效率，此模块主要还是用于异步编程，减少异步等待时间，提高效率
 def pool_example():
-    with Pool(processes=5) as p:
-        print(p.map(f, range(10)))  # 多线程版本的map函数，用法和map基本类似,当processes值为1的时候，就和map一样效果
+    with Pool(processes=1) as p:
+        print(p.map(f, range(5)))  # 多线程版本的map函数，用法和map基本类似,当processes值为1的时候，就和map一样效果
 
 
 def pool_example_two() -> object:
-    with Pool(processes=5) as p:
-        process = p.Process(target=lambda name: print(name), args=('bob',))
-        process.start()
-        process.join()
+    with Pool(processes=10) as p:
+        proce = p.Process(target=lambda name: _info(name), args=('bob',))  # 参数加逗号
+        proce.start()
+        proce.join()
+
+
+def pool_example_three() -> object:
+    with Pool(processes=10) as p:
+        p.apply_async(_info, args=('bob',))
+        p.apply_async(_info, args=('bob2',))
+        p.apply_async(_info, args=('bob3',))
+        p.close()
+        p.join()
 
 
 def _info(title):
@@ -31,6 +41,7 @@ def _info(title):
     print('process id:', os.getpid())
 
 
+# 创建一个子进程
 def process_example():
     p = Process(target=lambda name: _info(name), args=('bob',))
     p.start()
@@ -39,4 +50,6 @@ def process_example():
 
 if __name__ == '__main__':
     # pool_example()
-    pool_example_two()
+    # pool_example_two()
+    pool_example_three()
+    # process_example()
---
+++ PythonTutorial/Advance/multiprocessing_test.py
@@ -0,0 +1,27 @@
+from multiprocessing import Pool
+import os, time, random
+
+
+def long_time_task(name):
+    print('Run task %s (%s)...' % (name, os.getpid()))
+    start = time.time()
+    time.sleep(random.random() * 3)
+    end = time.time()
+    print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+
+
+def _info(title):
+    print(title)
+    print('module name:', __name__)
+    print('parent process:', os.getppid())
+    print('process id:', os.getpid())
+
+if __name__ == '__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Pool()
+
+    p.apply_async(_info, args=("bob",))
+    print('Waiting for all subprocesses done...')
+    p.close()
+    p.join()
+    print('All subprocesses done.')
