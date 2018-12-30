86436d16b3ed0527cfb1606c172c6d5b5f6f294d
Danielyan86 <516495459@qq.com>
2018-12-30 14:30:30 +0800

    update python

--- PythonTutorial/Basic/file_basic.py
+++ PythonTutorial/Basic/file_basic.py
@@ -1,31 +1,48 @@
 # 以写模式打开一个文件，如果不存在则创建新文件，如果存在，则可能清空原有内容
+def write_mode():
+    f = open("test", "w")
+    f.write("first line\n")
+    f.write("second line\n")
+    f.close()  # 关闭文件
 
-f = open("test", "w")
-f.write("first line\n")
-f.write("second line\n")
-f.close()  # 关闭文件
 
 # 以写追加模式打开一个文件，如果不存在则创建新文件，如果存在，则在原有内容后面追加内容
-f = open("test", "a")
-print("append")
-f.write("third line\n")
-f.write("four line\n")
-f.close()
+def append_mode():
+    f = open("test", "a")
+    print("append")
+    f.write("third line\n")
+    f.write("four line\n")
+    f.close()
+
 
 # 只读模式
 # readline 一次读一行
+def read_mode():
+    with open("test", "r") as f:
+        print(f.readline())
+        print(f.readline())
 
-with open("test", "r") as f:
-    print(f.readline())
-    print(f.readline())
 
 # 使用with关键字，打开文件方式更简洁
 # readlines 一次读完，返回list
-with open("test", "r") as f:
-    print(f.readlines())
+def use_with_method():
+    with open("test", "r") as f:
+        print(f.readlines())
+
 
 # 读写混合模式
-with open("test", "r+") as f:
-    print(f.readline())
-    print(f.readline())
-    f.write("read and write mode")
+def write_and_read_mixed_mode():
+    with open("test", "r+") as f:
+        print(f.readline())
+        print(f.readline())
+        f.write("read and write mode")
+
+
+def iter_mode():
+    with open("test", "r+") as f:
+        for line in f:
+            print(line)
+
+
+if __name__ == '__main__':
+    iter_mode()
