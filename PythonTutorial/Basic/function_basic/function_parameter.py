fcb07f336c68a5aaa09d3174fe1077d03ddad19a
Danielyan86 <516495459@qq.com>
2018-12-30 14:40:25 +0800

    update python

--- PythonTutorial/Basic/function_basic/function_parameter.py
+++ PythonTutorial/Basic/function_basic/function_parameter.py
@@ -1,49 +1,49 @@
 # 位置参数
-def positionParameter(who):
+def position_parameter(who):
     print("hello", who)
 
 
 # print(positionParameter())
-positionParameter("world")
+position_parameter("world")
 
 
 # 默认参数
-def taxme(cost, rate=0.08):
+def tax_me(cost, rate=0.08):
     return cost + rate * cost
 
 
-print(taxme(10))
+print(tax_me(10))
 
 
 # 非关键字可变长参数
 # *tunple贪婪匹配参数
-def tupleVarArgs(arg1, arg2=2, *tuple):
-    print(arg1, arg2, tuple)
-    for item in tuple:
+def tuple_var_args(arg1, arg2=2, *tuple_para):
+    print(arg1, arg2, tuple_para)
+    for item in tuple_para:
         print(item)
 
 
-tupleVarArgs(1, 2, 3, 4)
+tuple_var_args(1, 2, 3, 4)
 
 
 # 关键字变量参数
-def dictVarArgs(arg1, arg2=2, **theRest):
+def dict_var_args(arg1, arg2=2, **the_rest):
     print(arg1)
     print(arg2)
-    print(theRest)
-    for key in theRest:
-        print(theRest[key])
+    print(the_rest)
+    for key in the_rest:
+        print(the_rest[key])
 
 
 # 针对**theRest可以传入任意多的参数
-dictVarArgs(1, 2, key=3, key4=4)
+dict_var_args(1, 2, key=3, key4=4)
 
 
 # 参数拷贝问题
-def parmeter_copy(a={}):
+def parameter_copy(a={}):
     a['key2'] = 'new'
 
 
 b_dic = {"key1": 1}
-parmeter_copy(b_dic)
+parameter_copy(b_dic)
 print(b_dic)
