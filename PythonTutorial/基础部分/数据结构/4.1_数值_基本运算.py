2bc3d74313e441f10e7ea3e430cd1eda927553d4
Danielyan86 <516495459@qq.com>
2019-05-07 13:50:24 +0800

    update python

--- PythonTutorial/基础部分/数据结构/4.1_数值_基本运算.py
+++ PythonTutorial/基础部分/数据结构/4.1_数值_基本运算.py
@@ -1,15 +1,15 @@
-# 打印一个字符的十六进制
+# 打印一个字符的ASC或者Unicode值
 print(ord("A"))
 
 # 把整型数字转化成16进制
 hexadecimal = hex(10)
-print("打印16进制数字: {hexadecimal}".format(hexadecimal=hexadecimal))
+print("打印16进制字符: {hexadecimal}".format(hexadecimal=hexadecimal))
 
 bin_number = bin(10)
-print("打印2进制数字: {bin_number}".format(bin_number=bin_number))
+print("打印2进制字符: {bin_number}".format(bin_number=bin_number))
 
 oct_number = oct(10)
-print("打印8进制数字: {oct_number}".format(oct_number=oct_number))
+print("打印8进制字符: {oct_number}".format(oct_number=oct_number))
 
 # division always returns a floating spoint number
 print("8 / 5 = {0}".format(8 / 5))
@@ -28,7 +28,7 @@ print("2 | 1 = {0}".format(2 | 1))
 
 print("2 & 1 = {0}".format(2 & 1))
 
-print("3 & 1 = {0}".format(3 >> 1))
+print("3 >> 1 = {0}".format(3 >> 1))
 
 # 左移两位
 print(1 << 2)
