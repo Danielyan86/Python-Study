aab6e4e78868114054bd40d0e6d9f2a74476b6b9
Danielyan86 <516495459@qq.com>
2019-09-18 23:38:29 +0800

    update

---
+++ PythonTutorial/unit_test/example.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    return a + b
+
+
+def covert_result_to_string(a, b):
+    add_numbers = add(a, b)
+    return str(add_numbers)
---
+++ PythonTutorial/unit_test/example_test.py
@@ -0,0 +1,27 @@
+import unittest
+from unittest import mock, TestCase
+from unittest.mock import MagicMock,PropertyMock
+
+import example
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)
+
+    def test_add(self):
+        self.assertEqual(example.add(1, 1), 2)
+
+    @mock.patch('example.add')
+    def test_convert_result_to_string(self, add):
+        add.return_value = 4
+        self.assertEqual("4", example.covert_result_to_string(2, 2))
+
+    @mock.patch('example.add', new_callable=PropertyMock)
+    def test_convert_result_to_string_2(self, add):
+        add.return_value = 4
+        self.assertEqual("4", example.covert_result_to_string(2, 2))
+
+
+if __name__ == '__main__':
+    unittest.main()
