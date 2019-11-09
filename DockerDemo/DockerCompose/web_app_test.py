5dbb25c8563424623ec963f6971e6748854d7314
Danielyan86 <516495459@qq.com>
2019-11-09 20:31:55 +0800

    update

--- DockerDemo/DockerCompose/web_app_test.py
+++ DockerDemo/DockerCompose/web_app_test.py
@@ -2,13 +2,13 @@ import time
 
 from selenium import webdriver
 
-# DOCKER = False
-# current_time = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
-# logging.basicConfig(filename="./log/selenium_deb_log_{0}".format(current_time), level=logging.DEBUG)
-#
+# 在集群内访问另外一个容器
 
 url = "web-app:80"
-d = webdriver.Chrome()
+selenium_grid_url = "http://127.0.0.1:24444/wd/hub"
+capabilities = webdriver.DesiredCapabilities.CHROME.copy()
+d = webdriver.Remote(command_executor=selenium_grid_url, desired_capabilities=capabilities)
+
 
 d.get(url)
 time.sleep(6)
