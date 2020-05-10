3c32872116453ad8d669a7217048d47a619bace8
Danielyan86 <516495459@qq.com>
2020-05-10 11:02:49 +0800

    update

--- DockerDemo/AutomationTestDemo/unittest_demo/run_script_in_docker.sh
+++ DockerDemo/AutomationTestDemo/unittest_demo/run_script_in_docker.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # 脚本是在dockerfile里面通过add加进去的，所以执行路径应该和dockerfile添加路径一致
+# 需要设置脚本里面 REMOTE = False
 docker exec -i automation-container python /home/seluser/automation/myScript/bing_test.py
