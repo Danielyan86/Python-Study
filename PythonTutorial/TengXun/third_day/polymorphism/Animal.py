755d90a1cabccc3a9fdf8872bf19133416e15b61
danielyan86 <516495459@qq.com>
2021-10-29 16:12:47 +0800

    update

---
+++ PythonTutorial/TengXun/third_day/polymorphism/Animal.py
@@ -0,0 +1,31 @@
+class Animal():
+    def who(self):
+        print("I am an Animal")
+
+
+class Duck(Animal):
+    def who(self):
+        print("I am a duck")
+
+
+class Dog(Animal):
+    def who(self):
+        print("I am a dog")
+
+
+class Cat(Animal):
+    def who(self):
+        print("I am a cat")
+
+
+def func(obj):
+    obj.who()
+
+
+if __name__ == "__main__":
+    duck = Duck()
+    dog = Dog()
+    cat = Cat()
+    func(duck)
+    func(dog)
+    func(cat)
