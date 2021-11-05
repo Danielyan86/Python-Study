2fdc34f0278a7ed52d9330b008d817bf53da441e
Danielyan86 <516495459@qq.com>
2021-11-05 14:57:25 +0800

    update

--- PythonTutorial/Advance/multiple_thread_process/code/multiple_queue.py
+++ PythonTutorial/Advance/multiple_thread_process/code/multiple_queue.py
@@ -17,27 +17,28 @@ def queue_basic():
         print(q.get())
 
 
-class spider:
+class Spider:
     q = Queue()
 
     def __init__(self):
         self.req = requests.Session()
 
     def get_user_list(self):
-        url = "https://api.github.com/orgs/PlayPython/members?access_token=376d764af54ded124fe3f241309e090c5b58b18e"
+        url = "https://api.github.com/orgs/PlayPython/members?access_token="
         res = self.req.get(url)
+        res.raise_for_status()
         self.user_urls = [item['url'] for item in res.json()]
 
     def one_thread_get_public_repos(self):
         res_list = []
         for url in self.user_urls:
-            res = self.req.get(url + "?access_token=376d764af54ded124fe3f241309e090c5b58b18e")
+            res = self.req.get(url + "?access_token=")
             res_list.append(res.json()['public_repos'])
         print(res_list)
 
     def get_function(self, url):
         print(url)
-        res = self.req.get(url + "?access_token=376d764af54ded124fe3f241309e090c5b58b18e")
+        res = self.req.get(url + "?access_token=")
         print(res.json()['public_repos'])
         self.q.put(res.json()['public_repos'])
 
@@ -50,16 +51,16 @@ class spider:
         for thread in threads:
             thread.join()
 
-    def read_queue_write_to_file(self):
+    def write_content_to_file(self):
         while not self.q.empty():
             print(self.q.get())
             # print(q.get())
 
 
 if __name__ == '__main__':
-    spider_inst = spider()
+    spider_inst = Spider()
     spider_inst.get_user_list()
     # spider_inst.one_thread_get_public_repos()
 
     spider_inst.multiple_thread_get_public_repos()
-    spider_inst.read_queue_write_to_file()
+    spider_inst.write_content_to_file()
