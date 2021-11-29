1215791b417756477743661cc9f5cfdb9153ac6f
danielyan86 <516495459@qq.com>
2021-11-29 14:49:26 +0800

    update

--- PythonTutorial/Advance/multiple_threads_and_processes/code/processes/pipe_example_1.py
+++ PythonTutorial/Advance/multiple_threads_and_processes/code/processes/pipe_example_1.py
@@ -2,12 +2,13 @@ from multiprocessing import Process, Pipe
 
 
 def f(conn):
+    # conn 就是pipe传进来的对象
     conn.send([42, None, 'hello'])
     conn.close()
 
 
 if __name__ == '__main__':
-    parent_conn, child_conn = Pipe()
+    parent_conn, child_conn = Pipe()  # Pipe()函数返回一对由管道连接的连接对象，默认为双工（双向）。
     p = Process(target=f, args=(child_conn,))
     p.start()
     print(parent_conn.recv())  # prints "[42, None, 'hello']"
--- PythonTutorial/Advance/multiple_threads_and_processes/code/processes/server.py
+++ PythonTutorial/Advance/multiple_threads_and_processes/code/processes/server.py
@@ -1,26 +1,38 @@
 from multiprocessing import Process, Manager
 
 
-def cube(d, l):
-    d["car"] = "ford"
+def f(d, l):
+    d[1] = 1
+    d[2] = 2
+    d[3] = 3
+    l.reverse()
+
+
+def f2(d, l):
+    print(d)
+    for key in d.keys():
+        d[key] = key + 1
     l.sort()
 
 
-if __name__ == "__main__":
-    manager = Manager()
+if __name__ == '__main__':
+    with Manager() as manager:
+        d = manager.dict()
+        print(d)
+        l = manager.list(range(10))
+        print(l)
 
-    manager_dic = manager.dict()
-    manager_list = manager.list([9, 3])
+        p = Process(target=f, args=(d, l))
+        p.start()
+        p.join()
+        print(f"after first time change: {d}")
+        print(f"after first time change: {l}")
 
-    p = Process(
-        target=cube,
-        args=(
-            manager_dic,
-            manager_list,
-        ),
-    )
-    p.start()
-    p.join()
+        p = Process(target=f2, args=(d, l))
+        p.start()
+        p.join()
 
-    print(manager_dic)
-    print(manager_list)
+        print(f"final dictionary value: {d}")
+        print(f"final list value: {l}")
+# 服务器进程管理器比使用共享内存对象更灵活，因为它们可以被做成支持任意对象类型。
+# 而且，一个单一的管理器可以通过网络被不同计算机上的进程共享。然而，它们比使用共享内存要慢。
