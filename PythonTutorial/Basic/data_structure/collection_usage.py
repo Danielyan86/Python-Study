e67f7fd6ddc93657d733e8ea36535e55d56f8b81
Danielyan86 <516495459@qq.com>
2023-02-28 15:24:59 +0800

    Update

---
+++ PythonTutorial/Basic/data_structure/collection_usage.py
@@ -0,0 +1,26 @@
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                       for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+if '__main__' == __name__:
+    beer_card = Card(7, 'diamonds')
+    print(beer_card)
+
+    deck = FrenchDeck()
+    print(len(deck))
